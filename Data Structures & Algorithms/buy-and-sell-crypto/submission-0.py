fbb51ab6150ba3f084b534114d18c71406cf869e
Demo-Breeze <147712301+Demo-Breeze@users.noreply.github.com>
2026-08-13 10:12:13 -0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        s=len(prices)
+        i=[]
+        for char in range(s-1,-1,-1):
+            d = prices.pop(char)
+            l=min(prices,default=0)
+            print(l)
+            x= d-l
+            i.append(x)     
+        i.pop(-1)
+        print(i)
+        if all(b < 0 for b in i) ==True:
+            return 0
+        else:
+            return(max(i))
