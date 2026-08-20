612565404cf152fc5441a2340037ea2c31b01ca3
Demo-Breeze <147712301+Demo-Breeze@users.noreply.github.com>
2026-08-20 16:02:10 -0700

    Add: score-of-a-string - submission-2

---
+++ Data Structures & Algorithms/score-of-a-string/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        d= 0 
+        c =1
+        answer = []
+        while d < len(s)-1:
+            answer.append(abs(ord(s[c]) - ord(s[d])))
+            c+=1
+            d+=1
+        final_answer = sum(answer)
+        return final_answer
